9ecd0e6c3e5f348b025928fb981fcf38e186d630
Evelina Bortnyk <109087070+evelinavegas@users.noreply.github.com>
2023-03-28 17:01:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import {dataArray} from './data.js';
 import {checkRoomValue} from './task6.js'
+import {sendForm} from './task7.js'
 
 
 setTimeout(createMap, 2000)
 checkRoomValue();
+sendForm()
+
 
 

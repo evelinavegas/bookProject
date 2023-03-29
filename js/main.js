5208913d7bbc1608727149201f458ebda78e1d4f
Evelina Bortnyk <109087070+evelinavegas@users.noreply.github.com>
2023-03-29 15:01:19 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,32 @@ import {sendForm} from './task7.js'
 
 
 setTimeout(createMap, 2000)
-checkRoomValue();
+cconst sendData = async (url, data) => {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      
+    });
+    if (!response.ok) {
+      throw new Error(`Error`);
+    }
+  
+    return await response.json();
+  }
+function sendForm(){
+    const form = document.querySelector('.ad-form');
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault();
+        const formData = new FormData(form);
+        const dataForm = {};
+        for(const [key, value] of formData) {
+          dataForm[key] = value;
+        }
+        console.log(dataForm)
+        sendData('http://localhost:8000/photos', dataForm)
+        
+    })
+}
 sendForm()
 
 
